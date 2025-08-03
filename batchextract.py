85374c43f46572f8e46ded11f2acd1fdfff01685
twatadani <watadat-tky@umin.ac.jp>
2025-08-03 12:44:17 +0900

    インポート位置を調整

--- batchextract.py
+++ batchextract.py
@@ -2,12 +2,13 @@
 extractmp4をマルチスレッドで並列に実行するbatchextract関数を記述する
 '''
 
-from mp42mp3.extractmp3 import extractmp3
 import concurrent.futures
 import os
 
 def batchextract(cfdict: dict, mp4list: list[str]):
     ''' extractmp4を並列に実行する '''
+    from extractmp3 import extractmp3
+
 
     ffmpeg = cfdict['ffmpegpath']
 
@@ -20,4 +21,3 @@ def batchextract(cfdict: dict, mp4list: list[str]):
     concurrent.futures.wait(futures)
 
     return
-1   
