4fd023dbbab57936ff5e2f17bc726968eebe04e2
twatadani <watadat-tky@umin.ac.jp>
2025-08-03 10:34:36 +0900

    initial commit

---
+++ extractmp3.py
@@ -0,0 +1,25 @@
+''' extractmp3
+外部コマンドffmpegを呼び出してmp3を抽出する関数extractmp3を記述する
+'''
+
+import os.path
+import subprocess
+
+def extractmp3(ffmpegpath: str, mp4path: str, mp3path: str):
+    ''' ffmpegコマンドを呼び出してmp3を抽出する
+     mp4pathはファイル名、mp3pathはディレクトリ名を指定する '''
+    
+    mp4path = os.path.expandvars(os.path.expanduser(mp4path))
+    mp4filename = os.path.basename(mp4path)
+    mp4prefix = os.path.splitext(mp4filename)[0]
+
+    mp3filename = os.path.join(mp4prefix, '.mp3')
+    mp3fullname = os.path.join(mp3path, mp3filename)
+
+    ffmpegpath = os.path.expandvars(os.path.expanduser(ffmpegpath))
+
+    # ffmpegを実行
+    result = subprocess.run( [ffmpegpath, '-i ', mp4path, '-c:a mp3', mp3path] )
+
+    return result
+    
