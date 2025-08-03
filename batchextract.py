6b65195f083e7b9858640321771bbf4f4bdf4e3b
twatadani <watadat-tky@umin.ac.jp>
2025-08-03 12:16:35 +0900

    batchextractを実装

--- batchextract.py
+++ batchextract.py
@@ -1,3 +1,23 @@
 ''' batchextract.py
 extractmp4をマルチスレッドで並列に実行するbatchextract関数を記述する
 '''
+
+from mp42mp3.extractmp3 import extractmp3
+import concurrent.futures
+import os
+
+def batchextract(cfdict: dict, mp4list: list[str]):
+    ''' extractmp4を並列に実行する '''
+
+    ffmpeg = cfdict['ffmpegpath']
+
+    nthreads = os.cpu_count()-1
+    nthreads = max(1, nthreads)
+    ex = concurrent.futures.ThreadPoolExecutor(max_workers=nthreads)
+
+    futures = [ ex.submit(extractmp3(ffmpeg, mp4, cfdict['mp3path'])) for mp4 in mp4list ]
+
+    concurrent.futures.wait(futures)
+
+    return
+   
