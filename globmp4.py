da5b64942c4615bf8aa4c17051247cabede26d2c
twatadani <watadat-tky@umin.ac.jp>
2025-08-03 11:41:47 +0900

    initial commit

---
+++ globmp4.py
@@ -0,0 +1,14 @@
+''' globmp4.py
+mp4ファイルをリストアップするglobmp4関数を記述する
+'''
+
+from glob import glob
+import os.path
+
+def globmp4(cfdict: dict) -> list[str]:
+    ''' mp4ファイルをリストアップする '''
+    mp4path = cfdict['mp4path']
+    pathpattern = os.path.join(mp4path, '**/*.mp4/')
+
+    globbed = glob(pathpattern, recursive=True)
+    return globbed
