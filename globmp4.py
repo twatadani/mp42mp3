9202a1556f156943e00adb57400a41c6e9b77f95
twatadani <watadat-tky@umin.ac.jp>
2025-08-03 11:48:22 +0900

    typoを修正

--- globmp4.py
+++ globmp4.py
@@ -8,7 +8,7 @@ import os.path
 def globmp4(cfdict: dict) -> list[str]:
     ''' mp4ファイルをリストアップする '''
     mp4path = cfdict['mp4path']
-    pathpattern = os.path.join(mp4path, '**/*.mp4/')
+    pathpattern = os.path.join(mp4path, '**/*.mp4')
 
     globbed = glob(pathpattern, recursive=True)
     return globbed
