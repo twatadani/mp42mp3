55ac4e786af5f4b74af5bb2f2712e4825cf030dd
twatadani <watadat-tky@umin.ac.jp>
2025-08-03 11:32:59 +0900

    ファイル名のバグを修正

--- extractmp3.py
+++ extractmp3.py
@@ -13,13 +13,12 @@ def extractmp3(ffmpegpath: str, mp4path: str, mp3path: str):
     mp4filename = os.path.basename(mp4path)
     mp4prefix = os.path.splitext(mp4filename)[0]
 
-    mp3filename = os.path.join(mp4prefix, '.mp3')
+    mp3filename = mp4prefix + '.mp3'
     mp3fullname = os.path.join(mp3path, mp3filename)
-
     ffmpegpath = os.path.expandvars(os.path.expanduser(ffmpegpath))
 
     # ffmpegを実行
-    result = subprocess.run( [ffmpegpath, '-i ', mp4path, '-c:a mp3', mp3path] )
+    result = subprocess.run( [ffmpegpath, '-i', mp4path, '-c:a', 'mp3', mp3fullname] )
 
     return result
     
